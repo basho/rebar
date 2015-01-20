basho/rebar

commit b44b4f419934cfca59533d6f251b3f7d354ebbd9

Fix detection of .app.src.script (Reported-by: Vlad Dumitrescu)
rebar_app_utils:is_app_dir/1 failed to search for .app.src.script when
looking for an app file in src/.

While at it, fix inconsistent module export list.

src/rebar_app_utils.erl
@@ -26,7 +26,8 @@
 %% -------------------------------------------------------------------
 -module(rebar_app_utils).
 
--export([is_app_dir/0, is_app_dir/1,
+-export([is_app_dir/0,
+         is_app_dir/1,
          is_app_src/1,
          app_src_to_app/1,
          app_name/2,
@@ -47,7 +48,7 @@ is_app_dir() ->
 
 is_app_dir(Dir) ->
     SrcDir = filename:join([Dir, "src"]),
-    AppSrc = filename:join([SrcDir, "*.app.src"]),
+    AppSrc = filename:join([SrcDir, "*.app.{src,src.script}"]),
     case filelib:wildcard(AppSrc) of
         [AppSrcFile] ->
             {true, AppSrcFile};

commit 5f0ab0268efc691120fbd8cdca8c60f525b62c06

Fix .app.src.script bug introduced in b44b4f4
Handle the case when both .app.src and .app.src.script exist;
the script takes precedence.

inttest/app_src/app_src_rt.erl
@@ -1,5 +1,29 @@
 %% -*- erlang-indent-level: 4;indent-tabs-mode: nil -*-
 %% ex: ts=4 sw=4 et
+%% -------------------------------------------------------------------
+%%
+%% rebar: Erlang Build Tools
+%%
+%% Copyright (c) 2014 Vlad Dumitrescu
+%%
+%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%% of this software and associated documentation files (the "Software"), to deal
+%% in the Software without restriction, including without limitation the rights
+%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%% copies of the Software, and to permit persons to whom the Software is
+%% furnished to do so, subject to the following conditions:
+%%
+%% The above copyright notice and this permission notice shall be included in
+%% all copies or substantial portions of the Software.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+%% THE SOFTWARE.
+%% -------------------------------------------------------------------
 -module(app_src_rt).
 
 -compile(export_all).
@@ -44,7 +68,7 @@ has_line([L|T], RE) ->
     end.
 
 %%
-%% Generate the contents of a simple .app.src.script file
+%% Generate the contents of a simple .app.src file
 %%
 app(Name) ->
     "{application, " ++ atom_to_list(Name) ++ ",

inttest/app_src_script_2/app_src_script_2.erl
@@ -1,0 +1,6 @@
+-module(app_src_script_2).
+
+-compile(export_all).
+
+test() ->
+    ok.

inttest/app_src_script_2/app_src_script_2_rt.erl
@@ -24,7 +24,7 @@
 %% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 %% THE SOFTWARE.
 %% -------------------------------------------------------------------
--module(app_src_script_rt).
+-module(app_src_script_2_rt).
 
 -compile(export_all).
 
@@ -32,30 +32,32 @@
 
 files() ->
     [{copy, "../../rebar", "rebar"},
-     {create, "src/app_src_script.app.src.script", app_script(app_src_script)}].
+     {create, "src/app_src_script_2.app.src.script", app_script(app_src_script_2)},
+     {create, "src/app_src_script_2.app.src", app(app_src_script_2)}].
 
 run(Dir) ->
     retest_log:log(debug, "Running in Dir: ~s~n", [Dir]),
     {ok, [_Pid|Output]} = retest:sh("./rebar compile -vv",
                                     [{async, false}]),
 
-    Regexp = "DEBUG: Evaluating config script .*/app_src_script\.app\.src\.script.*",
+    Regexp = "DEBUG: Evaluating config script .*/app_src_script_2\.app\.src\.script.*",
     ?assertEqual(true, has_line(Output, Regexp)),
     retest_log:log(debug, "Evaluated .app.src.script~n", []),
 
     %% check that ebin/app_src.app exists
-    ?assertMatch(true, filelib:is_regular("ebin/app_src_script.app")),
+    ?assertMatch(true, filelib:is_regular("ebin/app_src_script_2.app")),
     retest_log:log(debug, "Generated ebin/.app~n", []),
 
-    %% check that ebin/.app has vsn="2"
-    {ok, Bin} = file:read_file("ebin/app_src_script.app"),
+    %% check that ebin/.app has vsn="2" (as in .script)
+    {ok, Bin} = file:read_file("ebin/app_src_script_2.app"),
     Str = binary_to_list(Bin),
+    retest_log:log(debug, "app=~p~n", [Str]),
     ?assertMatch({match, _}, re:run(Str, "{vsn, *\"2\"}")),
-    retest_log:log(debug, "Variable replacement in .app is ok.~n", []),
+    retest_log:log(debug, "app.src has version from script.~n", []),
 
-    %% check that ebin/.app doesn't have foo=ok
-    ?assertMatch(nomatch, re:run(Str, "{foo, *ok}")),
-    retest_log:log(debug, "app.src hasn't 'foo' config.~n", []),
+    %% check that ebin/.app has foo=ok (from .src)
+    ?assertMatch({match, _}, re:run(Str, "{foo, *ok}")),
+    retest_log:log(debug, "app.src has 'foo' config from .src.~n", []),
 
     ok.
 
@@ -75,9 +77,17 @@ has_line([L|T], RE) ->
 %% Generate the contents of a simple .app.src.script file
 %%
 app_script(Name) ->
-    "Vsn=\"2\".\n" ++
-        "{application, " ++ atom_to_list(Name) ++ ",
-           [{vsn, Vsn},
+	"[{application," ++ atom_to_list(Name) ++ ",Cfg}] = CONFIG,
+     [{application," ++ atom_to_list(Name) ++ ",lists:keyreplace(vsn, 1, Cfg,
+                                    {vsn, \"2\"})}].".
+
+%%
+%% Generate the contents of a simple .app.src file
+%%
+app(Name) ->
+    "{application, " ++ atom_to_list(Name) ++ ",
+           [{vsn, \"3\"},
+            {foo, ok},
             {modules, []},
-            {registered, []},
-            {applications, [kernel, stdlib]}]}.\n".
+             {registered, []},
+             {applications, [kernel, stdlib]}]}.\n".

src/rebar_app_utils.erl
@@ -47,27 +47,30 @@ is_app_dir() ->
     is_app_dir(rebar_utils:get_cwd()).
 
 is_app_dir(Dir) ->
-    SrcDir = filename:join([Dir, "src"]),
-    AppSrc = filename:join([SrcDir, "*.app.{src,src.script}"]),
-    case filelib:wildcard(AppSrc) of
-        [AppSrcFile] ->
-            {true, AppSrcFile};
-        [] ->
-            EbinDir = filename:join([Dir, "ebin"]),
-            App = filename:join([EbinDir, "*.app"]),
-            case filelib:wildcard(App) of
-                [AppFile] ->
-                    {true, AppFile};
-                [] ->
-                    false;
-                _ ->
-                    ?ERROR("More than one .app file in ~s~n", [EbinDir]),
-                    false
-            end;
-        _ ->
-            ?ERROR("More than one .app.src file in ~s~n", [SrcDir]),
-            false
-    end.
+	SrcDir = filename:join([Dir, "src"]),
+	AppSrcScript = filename:join([SrcDir, "*.app.src.script"]),
+	AppSrc = filename:join([SrcDir, "*.app.src"]),
+	case {filelib:wildcard(AppSrcScript), filelib:wildcard(AppSrc)} of
+		{[AppSrcScriptFile], _} ->
+			{true, AppSrcScriptFile};
+		{[], [AppSrcFile]} ->
+			{true, AppSrcFile};
+		{[],[]} ->
+			EbinDir = filename:join([Dir, "ebin"]),
+			App = filename:join([EbinDir, "*.app"]),
+			case filelib:wildcard(App) of
+				[AppFile] ->
+					{true, AppFile};
+				[] ->
+					false;
+				_ ->
+					?ERROR("More than one .app file in ~s~n", [EbinDir]),
+					false
+			end;
+		{_, _} ->
+			?ERROR("More than one .app.src file in ~s~n", [SrcDir]),
+			false
+	end.
 
 
 is_app_src(Filename) ->