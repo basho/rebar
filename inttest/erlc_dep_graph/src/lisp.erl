b0320ce058b0dfba056398b15d3803da755f5d7c
David Kubecka <davidkubecka@seznam.cz>
2015-03-28 12:53:20 +0100

    Basic test for erlc (re)compiling and erlcinfo

---
+++ inttest/erlc_dep_graph/src/lisp.erl
@@ -0,0 +1,8 @@
+%% -*- mode: erlang;erlang-indent-level: 4;indent-tabs-mode: nil -*-
+%% ex: ts=4 sw=4 ft=erlang et
+-module(lisp).
+
+-export([parse_transform/2]).
+
+parse_transform(Forms, _Options) ->
+    Forms.
