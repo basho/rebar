b816c69e343c8fd757c59fe8703eeda597f4da0a
Luis Rascão <luis.rascao@gmail.com>
2016-02-19 16:52:46 +0000

    Treat port env vars as expandable only if they self reference
    Provide additional port test case.
    Also, update newest OS X versions build flags.

---
+++ inttest/port2/c_src/test1.c
@@ -0,0 +1,13 @@
+#include "test1.h"
+
+#ifndef TEST1
+#error TEST1 is not defined!
+#endif
+
+#ifndef TEST2
+#error TEST2 is not defined!
+#endif
+
+#ifndef TEST3
+#error TEST3 is not defined!
+#endif
